f91447cd2fb9975f9b3cf0d438b7696e5e3ebeca
Martin Wolfe <martin.wolfe@us.ibm.com>
2016-12-05 12:30:35 -0600

    Basic request / parse5 integration

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 /*eslint-env node */
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var parse5 = require('parse5');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	
+	var queryUrl = "https://en.wikipedia.org/wiki/Second_Great_Awakening";
+	request(queryUrl, function(err, qRes, body) {
+		var document = parse5.parse(body);
+		
+		res.render('index', { title: 'Mechancial Turk', paragraph: document });
+	});
+
+	
+
 });
 
 module.exports = router;
