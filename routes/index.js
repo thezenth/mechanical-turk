ff2709734203f2e54aad0c045e204b6c633340ab
Martin Wolfe <martin.wolfe@us.ibm.com>
2016-12-05 18:45:04 -0600

    Attempting to parse classification as JSON

--- routes/index.js
+++ routes/index.js
@@ -27,10 +27,10 @@ router.get('/', function(req, res, next) {
 		    
 		    var thesaurusAPIKEY = "WPWGn8trAZDrMa8yrC0f";
 		    
-		    for (var noun in classed.nouns) {
+		    for (var noun in JSON.parse(classed.nouns)) {
 		    	
 		    	console.log("NOUN:" + noun);
-		    	console.log(classed.nouns);
+		    	console.log(JSON.parse(classed.nouns));
 		    	
 		    	var thesaurusQueryUrl = `http://thesaurus.altervista.org/thesaurus/v1?word=${noun}&language=en_US&key=${thesaurusAPIKEY}&output=json`;
 		    	console.log("THESAURUS URL:" + thesaurusQueryUrl);
